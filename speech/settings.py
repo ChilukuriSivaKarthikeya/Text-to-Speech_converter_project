18d06554da513106f3e3ab9f80c7a7979481fda1
unknown <chilukurisivakarthikeya@gmail.com>
2023-03-18 22:50:10 +0530

    initial commit

--- speech/settings.py
+++ speech/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^n&9f$q0*7y6w$1^n1rr6fzo^$qoqx*3$h0=*-y8($ahd7&*s-'
+SECRET_KEY = config('SECRET_KEY')
 #SECRET_KEY =os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #DEBUG = os.getenv('DEBUG','0').lower() in ['true','t','1']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
 
 
